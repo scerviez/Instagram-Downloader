9bdc228714a620c4b90ef88ea99434ccb73dc50c
Thiccy <74794033+phaticusthiccy@users.noreply.github.com>
2022-03-11 12:51:23 +0300

    2.0 Pre Build

--- index.js
+++ index.js
@@ -1,28 +1,67 @@
-const { Telegraf, Markup } = require('telegraf');
+const {
+  Telegraf,
+  Markup
+} = require('telegraf');
 const instagram = require("@phaticusthiccy/open-apis");
 const bot = new Telegraf(process.env.token);
 
-bot.start((ctx) => ctx.reply("Hai "+ctx.from.username+" Aku adalah bot untuk mendownload video/photo dari instagram. Silahkan Kirim link untuk memulai download", {
-      ...Markup.inlineKeyboard([
-        Markup.button.url('Developer', 
-           't.me/scerviez')
-      ])
-     }
-    ));
+bot.start(
+  (ctx) => ctx.reply(
+    "Hi " + ctx.from.username + " This bot can download videos, photos, IGTVs or reels from Instagram. Please send the link to start the download", {
+      ...Markup.inlineKeyboard([Markup.button.url('Developer', 't.me/scerviez')])
+    }
+  )
+)
 
 bot.on('text', (ctx) => {
-ctx.reply("Mendownload")
-let link = ctx.message.text;
-    instagram.insta_post(link).then(async (data) => {
-   if (data.post1.type = "mp4") {
-       ctx.replyWithVideo(data.post1.url)
-   
+  ctx.reply("Downloading..")
+  var link = ctx.message.text
+  try {
+    if (link.includes("/p/")) {
+      instagram.insta_post(link).then(async (data) => {
+        url = []
+        type = []
+        var sp = JSON.stringify(data).split("post")
+        var type2 = JSON.stringify(data).split("post")
+        var af2 = type2.shift()
+        var af = sp.shift()
+        type2.map((element2) => {
+          type.push(element2.split('"type":"')[1].split('"')[0])
+        })
+        sp.map((element) => {
+          url.push(element.split('"url":"')[1].split('"')[0])
+        })
+        var n = 0
+        url.forEach((send) => {
+          if (type[n] == "mp4") {
+            ctx.replyWithVideo(send)
+          } else {
+            ctx.replyWithPhoto(send)
+          }
+         n = n + 1
+        })
+      })
+    } else if (link.includes("reel")) {
+      try {
+        instagram.insta_reel(link).then(async (data) => {
+          return ctx.replyWithVideo(data.url)
+        })
+      } catch {
+        return ctx.reply("There is a problem with the given link. Check your url. (Profile must be open, not private)")
+      }
+    } else if (link.includes("tv")) {
+      try {
+        instagram.insta_post(link).then(async (data) => {
+          return ctx.replyWithVideo(data.post1.url)
+        })
+      } catch {
+        return ctx.reply("There is a problem with the given link. Check your url. (Profile must be open, not private)")
+      }
     } else {
-      ctx.replyWithPhoto(data.post1.url)
+      return ctx.reply("There is a problem with the given link. Check your url.")
     }
-  })
+  } catch {
+    return ctx.reply("There is a problem with the given link. Check your url. (Profile must be open, not private)")
+  }
 })
-
-
 bot.launch()
-
